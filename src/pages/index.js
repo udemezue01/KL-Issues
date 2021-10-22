d95ff76200bcf20f56be4c1846609da3fca8504e
Nwabufor John <johnzeus14@gmail.com>
2021-10-22 15:19:00 +0100

    updated react carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,47 @@
 import * as React from "react"
+import Slider from "react-slick";
 
 
 
 // markup
 const IndexPage = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+
+  
   return (
     <main>
 
-      <h1> This is the first Time</h1>
+      <h1 className="text-center text-4xl text-gray-800 tracking-wider"> This is the slider</h1>
+
+
+      <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
 
     </main>
   )
